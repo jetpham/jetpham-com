75c7c1e3910c56c992e0522ef6fc81031385cf36
Jet Pham <55770902+jetpham@users.noreply.github.com>
2025-03-31 16:36:13 -0700

    use x86_64-unknown-linux-musl targets

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 if ! command -v rustup
 then
   echo "Installing Rustup..."
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y default-host x86_64-unknown-linux-musl
   source "$HOME/.cargo/env"
+  rustup target add x86_64-unknown-linux-musl
 else
   echo "Rustup already installed."
 fi
@@ -16,7 +17,7 @@ if ! command -v trunk
 then
   echo "Installing trunk with binstall..."
   curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-  cargo binstall -y trunk
+  cargo binstall --targets x86_64-unknown-linux-musl -y trunk
   echo "Trunk installation complete."
 else
   echo "trunk already installed."
