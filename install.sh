a1c2febe88fbada6e0fa6762a4261f77e6ad7f03
Jet Pham <55770902+jetpham@users.noreply.github.com>
2025-03-31 16:09:11 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Install Rustup
+if ! command -v rustup
+then
+  echo "Installing Rustup..."
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  source "$HOME/.cargo/env"
+else
+  echo "Rustup already installed."
+fi
+
+# Install trunk using binstall
+if ! command -v trunk
+then
+  echo "Installing trunk with binstall..."
+  curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+  cargo binstall trunk
+  echo "Trunk installation complete."
+else
+  echo "trunk already installed."
+fi
+
+# Build project with trunk
+echo "Building project with trunk"
+trunk build
